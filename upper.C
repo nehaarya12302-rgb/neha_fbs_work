af14d79921488514c3683ba5ebd591d4df3ef6b9
nehaarya12302-rgb <nehaarya12302@gmail.com>
2025-09-29 16:53:53 +0530

    Add files via upload

---
+++ upper.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char ch = 'a';
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        printf("Uppercase");
+    }
+    else
+    {
+        printf("Lowercase");
+    }
+}
