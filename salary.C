60dc2a72ab3c36d34d204953a7dd96132c214938
nehaarya12302-rgb <nehaarya12302@gmail.com>
2025-09-30 08:25:39 +0530

    Add files via upload

---
+++ salary.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float basic = 5000, da, ta, hra, total;
+
+    if (basic <= 5000) {
+        da = basic * 0.10;
+        ta = basic * 0.20;
+        hra = basic * 0.25;
+    } else {
+        da = basic * 0.15;
+        ta = basic * 0.25;
+        hra = basic * 0.30;
+    }
+
+    total = basic + da + ta + hra;
+   	printf("Basic: %.2f  DA: %.2f  TA: %.2f  HRA: %.2f  Total: %.2f", basic, da, ta, hra, total);
+}
